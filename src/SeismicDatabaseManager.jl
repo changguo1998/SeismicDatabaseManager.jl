0ee3af383adb5c4e6a497ede9799043fad081ec9
Chang Guo <changguo1998@gmail.com>
2024-11-22 23:13:43 -0500

    move using commands to module root file

--- src/SeismicDatabaseManager.jl
+++ src/SeismicDatabaseManager.jl
@@ -1,6 +1,20 @@
+"""
+```julia
+module SeismicDatabaseManager
+```
+This module provides a high level data interface
+
+Direction definition
+- latitude / x positive to north
+- longitude / y positive to east
+- depth / z positive to down
+
+See `Source`, `Receiver`, `Event`, `GreenLibrary` for their usage
+"""
 module SeismicDatabaseManager
 
-using SeisTools
+using SeisTools, Dates, LengthAreaVolume, TOML
+
 
 _DP = SeisTools.DataProcess
 
